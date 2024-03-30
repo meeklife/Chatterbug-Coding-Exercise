6e8c995ceace2db55cb87a4afad753634052b467
meeklife <forsonbaah@gmail.com>
2024-03-30 14:28:47 +0000

    defined endpoint for generating password

--- main.py
+++ main.py
@@ -3,3 +3,17 @@ from script import PasswordRequest, generate_password
 import requests
 
 app = FastAPI()
+
+
+@app.post("/generate-password")
+def generate_password_endpoint(password_request: PasswordRequest):
+    try:
+        length = password_request.length
+        criteria1 = password_request.criteria1
+        criteria2 = password_request.criteria2
+
+        password = generate_password(length, criteria1, criteria2)
+
+        return {"password": password, "length": length}
+    except ValueError as err:
+        raise HTTPException(status_code=400, detail=str(err))
